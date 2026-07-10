55d535614a8ba2b51a6e4505ed94085e26918476
DESKTOP-G178GAD\yoti <vardankarapetyann2002@gmail.com>
2026-07-10 16:09:14 +0400

    HW01: add hello profile console app

---
+++ assignments/01-csharp-foundations/01-hello-profile/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Привет, как тебя зовут?");
+var name = Console.ReadLine();
+Console.WriteLine("Сколько тебе лет?");
+var age = Console.ReadLine();
+Console.WriteLine("Привет " + name + " тебе " + age + " лет");
+Console.ReadLine();
