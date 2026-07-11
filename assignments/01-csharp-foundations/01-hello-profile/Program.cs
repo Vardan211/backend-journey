307bdcdfffcdc30a15d3491631189963c2dd8cd5
DESKTOP-G178GAD\yoti <vardankarapetyann2002@gmail.com>
2026-07-11 14:38:24 +0400

    Complete hello profile validation

--- assignments/01-csharp-foundations/01-hello-profile/Program.cs
+++ assignments/01-csharp-foundations/01-hello-profile/Program.cs
@@ -1,6 +1,25 @@
 ﻿Console.WriteLine("Привет, как тебя зовут?");
 var name = Console.ReadLine();
+if (name =="")
+{
+    Console.WriteLine("Поле имени не может быть пустой");
+    Console.ReadLine();
+    return;
+}
 Console.WriteLine("Сколько тебе лет?");
-var age = Console.ReadLine();
-Console.WriteLine("Привет " + name + " тебе " + age + " лет");
+var agetext = Console.ReadLine();
+if (!int.TryParse(agetext, out int age))
+{
+    Console.WriteLine("Возраст должен быть числом");
+    Console.ReadLine();
+    return;
+}
+if (age <0 || age >120)
+{
+    Console.WriteLine("Возраст должен быть от 0 до 120");
+    Console.ReadLine();
+    return;
+}
+var age1 = age + 1;
+Console.WriteLine("Привет " + name + " тебе будет " + age1 + " через год");
 Console.ReadLine();
