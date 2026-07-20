58cd19163ea9e3d6e3ea615fdf0f4550e92c35e0
DESKTOP-G178GAD\yoti <vardankarapetyann2002@gmail.com>
2026-07-20 14:34:17 +0400

    hw-06-word-counter

---
+++ assignments/01-csharp-foundations/06-word-counter-dictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿//Попросить пользователя ввести текст
+//Проверить ввод
+//Если пользователь ввел пустую строку или только пробелы, вывести ошыбку
+Console.WriteLine("Напишите слова");
+string text = Console.ReadLine();
+var count = new Dictionary<string, int>();
+if (string.IsNullOrWhiteSpace(text))
+{
+    Console.WriteLine("Текст не должен быть пустым");
+}
+else
+{
+
+    string[] words = text.Split(' ');
+
+    foreach (string word in words)
+    {
+        string cleanWord = word.Trim().ToLower();
+        if (string.IsNullOrWhiteSpace(cleanWord))
+        {
+            continue;
+        }
+        if (count.ContainsKey(cleanWord))
+        {
+            count[cleanWord]++;
+        }
+        else
+        {
+            count[cleanWord] = 1;
+        }
+    }
+    Console.WriteLine("Результат:");
+    foreach (var pair in count)
+    {
+        Console.WriteLine($"{pair.Key}: {pair.Value}");
+    }
+}
